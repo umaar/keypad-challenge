c19194e9ed0712749159b51612f7c1507a1ac4f2
Umar <umar.hansa@gmail.com>
2016-05-23 22:28:28 -0700

    dots + lint

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const alphabet = [
 	['s', 't', 'u', 'v', 'w', 'x', 'y', 'z', '']
 ];
 
-const directions = ['up', 'right', 'down', 'left']
+const directions = ['up', 'right', 'down', 'left'];
 
 let currentX = 0;
 let currentY = 0;
@@ -15,24 +15,32 @@ function getSequence(word) {
 	let sequence = [];
 
 	word.split('').forEach(letter => {
-		for (let i=0; i<alphabet.length; i++) {
-			let row = alphabet[i];
+		for (let i = 0; i < alphabet.length; i++) {
+			const row = alphabet[i];
 
-			for (let j=0; j<row.length; j++) {
-				let char = alphabet[i][j];
+			for (let j = 0; j < row.length; j++) {
+				const char = alphabet[i][j];
+
 				if (char === letter) {
-
-					let targetX = j - currentX;
-					let targetY = i - currentY;
+					const targetX = j - currentX;
+					const targetY = i - currentY;
 
 					let direction;
-					if (targetX > 0) direction = directions[1];
-					if (targetX < 0) direction = directions[3];
-					sequence = sequence.concat( Array(Math.abs(targetX)).fill(direction) );
+					if (targetX > 0) {
+						direction = directions[1];
+					}
+					if (targetX < 0) {
+						direction = directions[3];
+					}
+					sequence = sequence.concat(Array(Math.abs(targetX)).fill(direction));
 
-					if (targetY > 0) direction = directions[2];
-					if (targetY < 0) direction = directions[0];
-					sequence = sequence.concat( Array(Math.abs(targetY)).fill(direction) );
+					if (targetY > 0) {
+						direction = directions[2];
+					}
+					if (targetY < 0) {
+						direction = directions[0];
+					}
+					sequence = sequence.concat(Array(Math.abs(targetY)).fill(direction));
 					sequence = sequence.concat('⏎');
 
 					currentX = j;
